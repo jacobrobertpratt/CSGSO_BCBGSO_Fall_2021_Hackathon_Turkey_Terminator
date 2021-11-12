3c0a70136916b29631f9157ec0afeed503a1c581
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-11-12 15:26:30 -0600

    Added PIE Words
    Added PIE Words for verb conjugation

--- translator.py
+++ translator.py
@@ -10,15 +10,16 @@ poses = ['pron', 'prep', 'root', 'adv', 'det', 'verb', 'adj', 'noun', 'conj']
 
 
 class DictionaryEntry:
-    def __init__(self, name: str, pos: str, oe_equiv: str, innovation: bool, pg_equiv: str):
+    def __init__(self, name: str, pos: str, oe_equiv: str, innovation: bool, pg_equiv: str, pie_equiv: str):
         self.name = name
         self.pos = pos
         self.oe_equiv = oe_equiv
         self.innovation = innovation
         self.pg_equiv = pg_equiv
+        self.pie_equiv = pie_equiv
 
     def __repr__(self):
-        return '{}[{}] <- {} <- {}'.format(self.name, self.pos, self.oe_equiv, self.pg_equiv)
+        return '{}[{}] <- {} <- {} <- {}'.format(self.name, self.pos, self.oe_equiv, self.pg_equiv, self.pie_equiv)
 
 
 def entry_from_json(s: str) -> Union[None, DictionaryEntry]:
@@ -26,6 +27,7 @@ def entry_from_json(s: str) -> Union[None, DictionaryEntry]:
     innovation = True
     oe_equiv = ''
     pg_equiv = ''
+    pie_equiv = ''
     if 'etymology_text' in datum:
         if 'from old english' in datum['etymology_text'].lower():
             innovation = False
@@ -33,10 +35,14 @@ def entry_from_json(s: str) -> Union[None, DictionaryEntry]:
             if m is not None:
                 oe_equiv = m.group('oe')
             if 'from proto-germanic' in datum['etymology_text'].lower():
-                m = re.search(r'Proto-Germanic \*(?P<pg>[^ ,]+)', datum['etymology_text'])
+                m = re.search(r'Proto-Germanic \*?(?P<pg>[^ ,]+)', datum['etymology_text'])
                 if m is not None:
                     pg_equiv = m.group('pg')
-        return DictionaryEntry(datum['word'], datum['pos'], oe_equiv, innovation, pg_equiv)
+            if 'from proto-indo-european' in datum['etymology_text'].lower():
+                m = re.search(r'Proto-Indo-European \*?(?P<pie>[^ ,]+)', datum['etymology_text'])
+                if m is not None:
+                    pie_equiv = m.group('pie')
+        return DictionaryEntry(datum['word'], datum['pos'], oe_equiv, innovation, pg_equiv, pie_equiv)
     return None
 
 
@@ -65,6 +71,6 @@ if __name__ == '__main__':
     w.prepare()
     print('Writing CSV file...')
     with open('./data/reduced_data.csv', 'wb+') as fp:
-        fp.write('modern_english,old_english,proto_germanic,innovation\n'.encode('utf8'))
-        fp.writelines(['"{}","{}","{}",{}\n'.format(d.name, d.oe_equiv, d.pg_equiv, d.innovation).encode('utf8') for d in w.data])
+        fp.write('modern_english,old_english,proto_germanic,proto_indo_european,innovation\n'.encode('utf8'))
+        fp.writelines(['"{}","{}","{}","{}",{}\n'.format(d.name, d.oe_equiv, d.pg_equiv, d.pie_equiv,  d.innovation).encode('utf8') for d in w.data])
     print('\n'.join(repr(d) for d in w.sample(100)))
