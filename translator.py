c34de75279b6689452ee5411d17af77403ea051c
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-11-12 15:32:09 -0600

    added pos
    added pos

--- translator.py
+++ translator.py
@@ -71,6 +71,7 @@ if __name__ == '__main__':
     w.prepare()
     print('Writing CSV file...')
     with open('./data/reduced_data.csv', 'wb+') as fp:
-        fp.write('modern_english,old_english,proto_germanic,proto_indo_european,innovation\n'.encode('utf8'))
-        fp.writelines(['"{}","{}","{}","{}",{}\n'.format(d.name, d.oe_equiv, d.pg_equiv, d.pie_equiv,  d.innovation).encode('utf8') for d in w.data])
+        fp.write('modern_english,old_english,proto_germanic,proto_indo_european,pos,innovation\n'.encode('utf8'))
+        fp.writelines(['"{}","{}","{}","{}","{}",{}\n'.format(d.name, d.oe_equiv, d.pg_equiv, d.pie_equiv,
+                                                              d.pos,  d.innovation).encode('utf8') for d in w.data])
     print('\n'.join(repr(d) for d in w.sample(100)))
