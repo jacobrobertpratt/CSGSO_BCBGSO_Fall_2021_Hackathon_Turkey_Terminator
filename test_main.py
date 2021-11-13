a2f218d503a53d05853b97bab32afda3303ab532
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-11-13 11:19:48 -0600

    Create test_main.py
    testing model predictions

---
+++ test_main.py
@@ -0,0 +1,20 @@
+import pickle
+import re
+from translator import embed, Phrase
+import numpy as np
+
+
+if __name__ == '__main__':
+    model_name = './models/ada_boost.sav'
+    with open(model_name, 'rb') as fp:
+        model = pickle.load(fp)
+    sentences = re.split(r'[.;"]', input('What to translate? '))
+    embeddings = embed(sentences).numpy()
+    phrases = []
+    for sentence, emb in zip(sentences, embeddings):
+        words = sentence.split()
+        for wi, word in enumerate(words):
+            phrases.append(Phrase.get_feature_vector(word, emb.tolist(), wi))
+    predictions = model.predict(np.array(phrases, dtype='float'))
+    oe_text = ' '.join([Phrase.vec2word(pred) for pred in predictions])
+    print(oe_text)
