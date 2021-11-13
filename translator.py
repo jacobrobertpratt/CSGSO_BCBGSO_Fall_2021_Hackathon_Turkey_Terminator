a19f001f3b38e1ab8186d21a1db8880b27ae66d5
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-11-13 09:12:41 -0600

    Update translator.py
    fixed embeddings key error issue

--- translator.py
+++ translator.py
@@ -73,9 +73,9 @@ def read_indices(path: pathlib.Path, embeddings: Dict[str, List[float]]) -> List
     with open(path, 'rb') as fp:
         data = fp.read()
         lines = data.decode('utf8').split('\n')
-        for i in tqdm(range(0, len(lines) - 3, 4)):
-            oe_sentence = lines[i]
-            ne_sentence = lines[i + 1]
+        for i in tqdm(range(0, len(lines) - 3, 4), desc="Reading word indices"):
+            oe_sentence = lines[i].strip()
+            ne_sentence = lines[i + 1].strip()
             edge_parts = lines[i + 2].split()
             edges = {}
             for e in range(0, len(edge_parts) - 1, 2):
