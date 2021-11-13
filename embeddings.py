23b6259cceedc75397f47938d8704577b6df8dde
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-11-12 22:31:29 -0600

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,6 +1,8 @@
 from translator import read_phrases
 import pathlib
+import tensorflow_hub as hub
 
 
 def create_word_dictionaries(path: pathlib.Path):
+    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
 
