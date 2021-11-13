474507bc42c710f7a4dae1b7d8c995ddb9d1d197
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-11-13 09:46:49 -0600

    Update trainer.py
    Added knn model for baseline

--- trainer.py
+++ trainer.py
@@ -1,6 +1,10 @@
 from translator import generate_phrase_objects, Phrase
 from typing import List, Tuple
 from tqdm import tqdm
+from sklearn import tree, neighbors
+from sklearn.model_selection import train_test_split
+import numpy as np
+import os
 
 
 def generate_dataset(phr: List[Phrase]) -> List[Tuple[List[float], List[float]]]:
@@ -11,7 +15,26 @@ def generate_dataset(phr: List[Phrase]) -> List[Tuple[List[float], List[float]]]
     return result
 
 
+def train_knn(train: Tuple[List[List[float]], List[List[float]]], test: Tuple[List[List[float]], List[List[float]]],
+              k: int = 5):
+    trainx, trainy = train
+    print('KNN model with k={}:'.format(k))
+    knn = neighbors.KNeighborsRegressor(k, n_jobs=os.cpu_count())
+    print('Fitting Model...')
+    model = knn.fit(trainx, trainy)
+    print('Training Accuracy: {}%'.format(model.score(trainx, trainy)))
+    testx, testy = test
+    print('Inference Accuracy: {}%'.format(model.score(testx, testy)))
+
+
 if __name__ == '__main__':
     phrases = generate_phrase_objects('./data/embedded_phrases.csv', './data/test_reduced_phrases_w_indices.txt')
     dataset = generate_dataset(phrases)
     print('Generated a dataset with {} entries'.format(len(dataset)))
+    X = [d[0] for d in dataset]
+    X = np.array(X, dtype='float')
+    Y = np.array([d[1] for d in dataset], dtype='float')
+    trainx, testx, trainy, testy = train_test_split(X, Y, test_size=0.2)
+    train = (trainx, trainy)
+    test = (testx, testy)
+    train_knn(train, test)
