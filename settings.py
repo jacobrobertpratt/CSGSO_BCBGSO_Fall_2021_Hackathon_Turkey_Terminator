e0e9d4bc791effa71a6c282f674e74e63c84373d
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-11-16 12:01:33 -0600

    refactoring for clean up

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 import os.path as path
 
-main_home = 'C:\\.budget'
-config_path = path.join(main_home, 'config.ini')
+main_home = 'C:\\.ne_oe'
 date_time_format = '%Y-%m-%d.%H-%M-%S'
 time_format = '%H-%M-%S'
 date_format = '%Y-%m-%d'
-database_path = path.join(main_home, 'accounts.db')
+database_path = path.join(main_home, 'words.db')
+model_path = path.join(main_home, 'models')
+data_path = path.join(main_home, 'data')
